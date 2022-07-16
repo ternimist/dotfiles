228661de7a82967bec22d556cfeb42d3758ea388
ternimist <ternimist@proton.me>
2022-07-16 19:00:24 +0700

    feat: add catppuccin theme

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,7 +1,23 @@
 local wezterm = require 'wezterm';
+local catppuccin = require("colors/catppuccin").setup {}
 
 return {
-  font = wezterm.font("Courier Prime Code"),
+  -- font
+  font = wezterm.font("FiraCode Nerd Font"),
   font_size = 14.0, 
+
+  -- colors
+  colors = catppuccin,
+
+  -- terminal_area
+  window_padding = {
+    left = 20,
+    right = 20,
+    top = 10,
+    bottom = 10,
+  },
+
+  -- spawn a fish shell in login mode
+  default_prog = {"/usr/local/bin/fish", "-l"},
 }
 
