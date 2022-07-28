0b96a2c985d1c3614f6a90f532ed6b589c5795b5
ternimist <ternimist@proton.me>
2022-07-29 01:09:26 +0700

    feat: add more lsp by mason.nvim

---
+++ dot_config/nvim/lua/custom/chadrc.lua
@@ -0,0 +1,39 @@
+-- Just an example, supposed to be placed in /lua/custom/
+
+local M = {}
+
+-- make sure you maintain the structure of `core/default_config.lua` here,
+-- example of changing theme:
+
+M.ui = {
+  theme = "catppuccin",
+}
+
+M.plugins = {
+  override = {
+     ["williamboman/mason.nvim"] = {
+      ensure_installed = {
+        -- lua stuff
+        "lua-language-server",
+        "stylua",
+
+        -- web dev
+        "css-lsp",
+        "html-lsp",
+        "typescript-language-server",
+        "deno",
+        "emmet-ls",
+        "json-lsp",
+
+        -- shell
+        "shfmt",
+        "shellcheck",
+      },
+    },
+  },
+}
+
+M.mappings = {}
+
+
+return M
