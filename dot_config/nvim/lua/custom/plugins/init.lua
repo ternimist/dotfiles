12ee5bd525087ef60a96f6fad188d0cebeac2d53
ternimist <ternimist@proton.me>
2022-08-01 15:41:39 +0700

    fix: change path to lspconfig

--- dot_config/nvim/lua/custom/plugins/init.lua
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -1,14 +1,8 @@
 return {
-  ["williamboman/mason-lspconfig.nvim"] = {
-    after = "mason.nvim",
-    config = function ()
-      require "custom.plugins.configs.masonlspconfig"
-    end,
-  },
   ["neovim/nvim-lspconfig"] = {
     config = function()
       require "plugins.configs.lspconfig"
-      require "custom.plugins.lspconfig"
+      require "custom.plugins.configs.lspconfig"
     end,
   },
 }
